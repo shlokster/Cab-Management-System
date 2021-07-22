04f6427fd4ba69224ccb73bf1fa9f71bfc5ed008
vanessaf21 <59829956+vanessaf21@users.noreply.github.com>
2021-07-23 01:13:48 +0530

    Added numOfDrivers

--- DriverLibrary.java
+++ DriverLibrary.java
@@ -1,55 +1,51 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
+public class DriverLibrary {
+	private Driver[] Drivers = new Driver[100];
+	public DriverLibrary(){}
+	private int numOfDrivers;
+	public DriverLibrary(String filename) {
 
-	import java.io.BufferedReader;
-	import java.io.FileReader;
-	import java.io.IOException;
+		try {
+			int count = 0;
 
-	public class DriverLibrary {
-		private Driver[] Drivers = new Driver[100];
+			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(fr);
 
-		public DriverLibrary() {
-			
-		}
-		public Driver[] getDrivers() {
-			return Drivers;
-		}
+			while ((br.readLine() != null)) {
 
-		public void driverdetails(String filename) {
-		
-			try {
+				String data = br.readLine();
+				String[] records = data.split("[,]");
+				int driverID = Integer.parseInt(records[0]);
+				String name = records[1], location = records[2], type = records[3];
+				long PhNum = Long.parseLong(records[4]);
+				int age = Integer.parseInt(records[5]);
+				int cabID = Integer.parseInt(records[6]);
+				boolean gender = Boolean.parseBoolean(records[7]);
+				float rating = Float.parseFloat(records[8]);
 
-				FileReader fr = new FileReader(filename);
-				BufferedReader br = new BufferedReader(fr);
+				Drivers[count] = new Driver(driverID, name, location, type, PhNum, age, cabID, gender, rating);
+				count++;
+				numOfDrivers = count;
 
-				while ((br.readLine() != null)) {
-
-					String data = br.readLine();
-					String[] records = data.split("[,]");
-					int driverID = Integer.parseInt(records[0]);
-					String name = records[1], location = records[2], type = records[3];
-					long PhNum = Long.parseLong(records[4]);
-					int age = Integer.parseInt(records[5]);
-					int cabID = Integer.parseInt(records[6]);
-					boolean gender = Boolean.parseBoolean(records[7]);
-					float rating = Float.parseFloat(records[8]);
-					int count = 0;
-	                Drivers[count] = new Driver(driverID, name, location, type, PhNum, age, cabID, gender, rating);
-					count++;
-					
-				}
-
-			} catch (IOException ioe) {
-				ioe.printStackTrace();
 			}
-		}
 
-		public static void main(String[] args) {
-			DriverLibrary x = new DriverLibrary();
-			String csvFile = "Driver.csv";
-			x.driverdetails(csvFile);
-
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
 
 	}
-	
+	public Driver[] getDrivers() {
+		return Arrays.copyOf(Drivers, 25);
+	}
+
+	public int getnumOfDrivers(){
+		return numOfDrivers;
+	}
+
+
+}
